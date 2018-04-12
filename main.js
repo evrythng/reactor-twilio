bf8b57217d6431cda59e88b7a0c5b9e3dee44995
C-D-Lewis <Bonsitm@gmail.com>
2018-04-12 11:17:02 +0100

    eslint main.js

--- main.js
+++ main.js
@@ -9,17 +9,17 @@ const AUTH_TOKEN = '';
 const PHONE_NUMBER = '';
 
 const PROPERTY_RULES = [{
-  condition: 'temperature_celsius > 0',
+  condition: 'temperature_celsius > 100',
   message: {
     to: '+447793248002',
-    body: 'Temperature exceeded acceptable levels!'
-  }
+    body: 'Temperature exceeded acceptable levels!',
+  },
 }, {
   condition: 'weather_report includes rain',
   message: {
     to: '+447793248002',
-    body: 'Rain is forecast!'
-  }
+    body: 'Rain is forecast!',
+  },
 }];
 
 
@@ -27,7 +27,7 @@ const PROPERTY_RULES = [{
 
 const sendSMS = ({ to, body }) => {
   const client = new TwilioClient(ACCOUNT_SID, AUTH_TOKEN);
-  
+
   logger.info(`Sending SMS: to=${to} body=${body}`);
   return client.messages.create({ to, from: PHONE_NUMBER, body });
 };
@@ -49,11 +49,11 @@ const runRule = (rule, update) => {
     '==': val => val == ruleValue,
     '<': val => val < ruleValue,
     '<=': val => val <= ruleValue,
-    'includes': val => `${val}`.includes(ruleValue),
-    '!=': val => val != ruleValue
+    includes: val => `${val}`.includes(ruleValue),
+    '!=': val => val != ruleValue,
   };
   if (!handlers[condition]) throw new Error(`Invalid rule condition: ${condition}`);
-  if(!handlers[condition](update.newValue)) return Promise.resolve();
+  if (!handlers[condition](update.newValue)) return Promise.resolve();
 
   return sendSMS(rule.message);
 };
@@ -62,7 +62,7 @@ const checkRules = (event) => {
   const update = event.changes;
   const updatedKeys = Object.keys(update);
   const promises = updatedKeys.map((key) => {
-    const rule = PROPERTY_RULES.find(rule => rule.condition.includes(key));
+    const rule = PROPERTY_RULES.find(item => item.condition.includes(key));
     if (!rule) return Promise.resolve();
 
     logger.info(`Running rule ${rule.condition}`);
