577759f50827b0509af8ef2caf2053885d736cfc
C-D-Lewis <Bonsitm@gmail.com>
2018-04-12 11:30:16 +0100

    More minor code improvements

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const TwilioClient = require('twilio');
 
-
 // ------------------------------- Configuration -------------------------------
 
 // Twilio Account SID, Auth Token, and phone number from the Twilio console
@@ -11,36 +10,30 @@ const PHONE_NUMBER = '';
 const PROPERTY_RULES = [{
   condition: 'temperature_celsius > 100',
   message: {
-    to: '+447793248002',
+    to: '',
     body: 'Temperature exceeded acceptable levels!',
   },
 }, {
   condition: 'weather_report includes rain',
   message: {
-    to: '+447793248002',
+    to: '',
     body: 'Rain is forecast!',
   },
 }];
-
 
 // ---------------------------------- Twilio -----------------------------------
 
 const sendSMS = ({ to, body }) => {
   const client = new TwilioClient(ACCOUNT_SID, AUTH_TOKEN);
-
   logger.info(`Sending SMS: to=${to} body=${body}`);
   return client.messages.create({ to, from: PHONE_NUMBER, body });
 };
 
-
 // ----------------------------------- Rules -----------------------------------
-
-const CONDITION_NUM_SECTIONS = 3;
 
 const runRule = (rule, update) => {
   const sections = rule.condition.split(' ');
-  if (sections.length !== CONDITION_NUM_SECTIONS) throw new Error('Invalid rule');
-  if (update.newValue === update.oldValue) return Promise.resolve();
+  if (sections.length !== 3) throw new Error('Invalid rule');
 
   const [key, condition, ruleValue] = sections;
   const handlers = {
@@ -60,17 +53,14 @@ const runRule = (rule, update) => {
 
 const checkRules = (event) => {
   const update = event.changes;
-  const updatedKeys = Object.keys(update);
-  const promises = updatedKeys.map((key) => {
+  return Promise.all(Object.keys(update).map((key) => {
     const rule = PROPERTY_RULES.find(item => item.condition.includes(key));
     if (!rule) return Promise.resolve();
 
     logger.info(`Running rule ${rule.condition}`);
     return runRule(rule, update[key]);
-  });
-  return Promise.all(promises);
+  }));
 };
-
 
 // ------------------------------- Reactor Events ------------------------------
 
